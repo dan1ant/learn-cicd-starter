2bc6745ebe8828c25928428e7f1029168b392ec5
dan1ant <21098537+dan1ant@users.noreply.github.com>
2025-05-28 17:17:11 +0200

    fix formatting

--- handler_ready.go
+++ handler_ready.go
@@ -2,5 +2,6 @@ package main
 
 import "net/http"
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request){
-	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})}
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
+}
